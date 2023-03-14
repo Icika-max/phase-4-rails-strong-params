0851310bd1d008787c54bebc92a09cb7422d9971
icika-max <icikakennedy@gmail.com>
2023-03-14 09:40:56 +0300

    competed

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,16 @@
 class Bird < ApplicationRecord
+      # POST /birds
+  def create
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+  # other controller actions here
+
+  private
+  # all methods below here are private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
 end
